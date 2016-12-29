48b85a6d3ddd8a0db3810b69ecc84bafdbbd37eb
Joakim Andén <janden@math.princeton.edu>
2016-12-28 21:41:25 -0500

    Add test script for nudftd_mx

---
+++ test_nudftd_mx.m
@@ -0,0 +1,26 @@
+% Script to compare the MATLAB and MEX implementations of the NUDFT.
+
+% Parameters.
+N = 64;
+n = 2^10;
+d = 3;
+
+% Generate sample data.
+rand('state', 0);
+omega = N/2*(rand(n, d)-0.5);
+alpha = rand(n, 1) + 1i*rand(n, 1);
+
+% MATLAB implementation: nudftd.
+tmr = tic;
+f0 = nudftd(N, omega, alpha);
+tm0 = toc(tmr);
+
+% MEX implementation in C: nudftd_mx.
+tmr = tic;
+f1 = nudftd_mx(N, omega, alpha);
+tm1 = toc(tmr);
+
+% Output results.
+fprintf('%-20s %f s\n', 'nudftd:', tm0);
+fprintf('%-20s %f s\n', 'nudftd_mx:', tm1);
+fprintf('%-20s %g\n', 'Relative error:', norm(f0(:)-f1(:))/norm(f0(:)));
