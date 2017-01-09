05e6129c553ee44c9cf7b2474422944a1060d297
Joakim Andén <janden@math.princeton.edu>
2017-01-08 21:05:32 -0500

    Also test MEX-augmented NUFFT in test_snufftd

--- test_snufftd.m
+++ test_snufftd.m
@@ -15,45 +15,52 @@ alpha = rand(n, 1) + 1i*rand(n, 1);
 tmr = tic;
 f0 = nudftd(N, omega, alpha);
 tm0 = toc(tmr);
-fprintf('%-10sTime: %15f s\n', 'NUDFT', tm0);
+fprintf('%-15sTime: %15f s\n', 'NUDFT', tm0);
 
 % Standard non-uniform FFT.
 tmr = tic;
 f1 = nufftd(N, omega, alpha);
 tm1 = toc(tmr);
 err1 = norm(f0(:)-f1(:));
-fprintf('%-10sTime: %15f s    Error: %15g\n', 'NUFFT', tm1, err1);
+fprintf('%-15sTime: %15f s    Error: %15g\n', 'NUFFT', tm1, err1);
+
+% MEX-augmented non-uniform FFT.
+tmr = tic;
+f2 = nufftd(N, omega, alpha, [], [], [], true);
+tm2 = toc(tmr);
+err2 = norm(f0(:)-f2(:));
+fprintf('%-15sTime: %15f s    Error: %15g\n', 'NUFFT (MEX)', tm2, err2);
 
 % Compact non-uniform FFT.
 tmr = tic;
-f2 = snufftd(N, omega, alpha);
-tm2 = toc(tmr);
-err2 = norm(f0(:)-f2(:));
-fprintf('%-10sTime: %15f s    Error: %15g\n', 'CNUFFT', tm2, err2);
+f3 = snufftd(N, omega, alpha);
+tm3 = toc(tmr);
+err3 = norm(f0(:)-f3(:));
+fprintf('%-15sTime: %15f s    Error: %15g\n', 'CNUFFT', tm3, err3);
 
 if exist('nufft1d1') && d <= 3
     if d == 1
         tmr = tic;
-        f3 = n*nufft1d1(n, ...
+        f4 = n*nufft1d1(n, ...
             2*pi/N*omega(:,1), ...
             alpha, 1, 1e-10, N);
-        tm3 = toc(tmr);
-        fun3 = 'nufft1d1';
+        tm4 = toc(tmr);
+        fun4 = 'nufft1d1';
     elseif d == 2
         tmr = tic;
-        f3 = n*nufft2d1(n, ...
+        f4 = n*nufft2d1(n, ...
             2*pi/N*omega(:,1), 2*pi/N*omega(:,2), ...
             alpha, 1, 1e-10, N, N);
-        tm3 = toc(tmr);
-        fun3 = 'nufft2d1';
+        tm4 = toc(tmr);
+        fun4 = 'nufft2d1';
     elseif d == 3
         tmr = tic;
-        f3 = n*nufft3d1(n, ...
+        f4 = n*nufft3d1(n, ...
             2*pi/N*omega(:,1), 2*pi/N*omega(:,2), 2*pi/N*omega(:,3), ...
             alpha, 1, 1e-10, N, N, N);
-        tm3 = toc(tmr);
-        fun3 = 'nufft3d1';
+        tm4 = toc(tmr);
+        fun4 = 'nufft3d1';
     end
-    err3 = norm(f0(:)-f3(:));
-    fprintf('%-10sTime: %15f s    Error: %15g\n', fun3, tm3, err3);
+    err4 = norm(f0(:)-f4(:));
+    fprintf('%-15sTime: %15f s    Error: %15g\n', fun4, tm4, err4);
 end
