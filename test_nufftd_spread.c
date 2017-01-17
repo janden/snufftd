d885349513c124a3f979bd7c6aea69f26ff9a523
Joakim Andén <janden@math.princeton.edu>
2017-01-17 14:01:06 -0500

    New testing program
    More flexible in selecting dimensions.

--- test_nufftd_spread.c
+++ test_nufftd_spread.c
@@ -2,34 +2,60 @@
 
 #include "nufftd_spread.h"
 
-#define         N       32
-#define         D       3
-#define         M       1024
+#define         N       64
+#define         D       2
+#define         M       16*N*N
 
 int main()
 {
-    double tau_re[(2*N)*(2*N)*(2*N)], tau_im[(2*N)*(2*N)*(2*N)];
+    double *tau_re, *tau_im, *omega, *alpha_re, *alpha_im;
 
-    double omega[D*M];
+    int k, i;
 
-    double alpha_re[M], alpha_im[M];
+    if(D == 1)
+    {
+        tau_re = calloc(2*N, sizeof(double));
+        tau_im = calloc(2*N, sizeof(double));
+    }
+    else if(D == 2)
+    {
+        tau_re = calloc(2*N*2*N, sizeof(double));
+        tau_im = calloc(2*N*2*N, sizeof(double));
+    }
+    else if(D == 3)
+    {
+        tau_re = calloc(2*N*2*N*2*N, sizeof(double));
+        tau_im = calloc(2*N*2*N*2*N, sizeof(double));
+    }
 
-    int k;
+    omega = calloc(M*D, sizeof(double));
+
+    alpha_re = calloc(M, sizeof(double));
+    alpha_im = calloc(M, sizeof(double));
 
     srand(0);
 
-    for(k = 0; k < D*M; k++)
+    for(i = 0; i < 50; i++)
     {
-        omega[k] = N/2*(rand()/RAND_MAX-0.5);
+        for(k = 0; k < D*M; k++)
+        {
+            omega[k] = N/2*(rand()/RAND_MAX-0.5);
+        }
+
+        for(k = 0; k < M; k++)
+        {
+            alpha_re[k] = rand()/RAND_MAX;
+            alpha_im[k] = rand()/RAND_MAX;
+        }
+
+        nufftd_spread(tau_re, tau_im, N, M, D, omega, alpha_re, alpha_im, 1.5629, 28, 2);
     }
 
-    for(k = 0; k < M; k++)
-    {
-        alpha_re[k] = rand()/RAND_MAX;
-        alpha_im[k] = rand()/RAND_MAX;
-    }
-
-    nufftd_spread(tau_re, tau_im, N, M, D, omega, alpha_re, alpha_im, 1.5629, 28, 2);
+    free(tau_re);
+    free(tau_im);
+    free(omega);
+    free(alpha_re);
+    free(alpha_im);
 
     return 0;
 }
