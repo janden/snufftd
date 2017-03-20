fb0569532a35838f76eab947d39c06162c34e077
Joakim Andén <janden@math.princeton.edu>
2017-03-20 15:01:44 -0400

    Fix Octave compatibility in make.m script
    Cannot use -largeArrayDims with mkoctfile.

--- make.m
+++ make.m
@@ -1,5 +1,11 @@
-mex -largeArrayDims nufftd_spread_mx.c nufftd_spread.c
+if isoctave()
+    mex nufftd_spread_mx.c nufftd_spread.c
 
-mex -largeArrayDims sub_snufftd_spread_mx.c sub_snufftd_spread.c
+    mex sub_snufftd_spread_mx.c sub_snufftd_spread.c
+else
+    mex -largeArrayDims nufftd_spread_mx.c nufftd_spread.c
+
+    mex -largeArrayDims sub_snufftd_spread_mx.c sub_snufftd_spread.c
+end
 
 clear functions;
